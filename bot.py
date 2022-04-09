d83a91e5af60fbfad8ebc7bc7e62a0efa8636356
licykk <licykk@gmail.com>
2022-04-09 06:47:42 -0400

    basic command stuff

--- bot.py
+++ bot.py
@@ -3,19 +3,30 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
 async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-client.run(TOKEN)
+
+@bot.command(name='hi', help='responds with :flushed:')
+async def send_flushed(ctx):
+    response = ":flushed:"
+    await ctx.channel.send(response)
+
+
+bot.run(TOKEN)
