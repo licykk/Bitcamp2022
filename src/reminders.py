bf1c074b5e59b05c537d16b8fcfcd7d77a288ed1
Hana Zherka <akrehzgmail.com>
2022-04-10 02:08:14 -0400

    reminders but the dms don't work

--- src/reminders.py
+++ src/reminders.py
@@ -6,37 +6,47 @@ import time
 
 #remind @blank 0m0d before task OR date/time with MESSAGE
 
-
+client = discord.Client()
 
 class Reminder(commands.Cog):
 
-    def __init__(self,bot, input):
+    def __init__(self,bot):
         self.bot = bot
-        client = discord.Client()
 
-    @commands.command
-    async def on_message(self, message):
-        if message.content.startswith('!remind'):
-            channel = message.channel
-            await channel.send('Say hello!')
+    @commands.command(name = 'remind')
+    async def remind(self,ctx):
+        await ctx.send('okay what reminder do you want?')
+        def check(input1):
+            input1 = input1.content
+            print(input1)
+            self.ex = re.search('to ("(?P<message>.*)")? on (?P<date>\d\d\/\d\d\/\d\d).*at (?P<time>\d\d?:\d\d) ?(?P<apm>pm|am)',input1)
+            self.recievers = re.findall('@(\S*)', input1)
+            print(self.ex)
+            print(self.recievers)
+            return ((self.ex != None) and (self.recievers != None)) or input1 == 'q'
 
-        def check(input):
-            self.ex = re.match('to ("(?P<message>.*)")? on (?P<date>\d\d\/\d\d\/\d\d).*at (?P<time>\d\d:\d\d) ?(?P<apm>pm|am)',input)
-            self.recievers = re.findall('@(\S*)', input)
-            return (self.ex != None) and (self.recievers != None)
+        msg = await self.bot.wait_for('message', check=check)
+        if msg.content=='q':
+            await ctx.send('woooowww you don\'t want a reminder? enjoy your f')
+        else:
+            date_ = DT.datetime.strptime(self.ex.group('date'), '%m/%d/%y')
+            time_ = DT.datetime.strptime(self.ex.group('time')+self.ex.group('apm'), '%I:%M%p')
+            self.dt = DT.datetime.combine(date_.date(), time_.time())
+            self.message = self.ex.group('message')
 
-        msg = await client.wait_for('message', check=check)
+            while self.dt < DT.datetime.now():
+                print('here')
+                time.sleep(1)
+            embeded = discord.Embed(title=self.message)
 
-        date = DT.datetime.strptime(self.ex.group('date'), '%m/%d/%y')
-        time = DT.datetime.strptime(self.ex.group('time')+self.ex.group('apm'), '%I:%M%p')
-        self.dt = DT.datetime.combine(date, time)
-        self.message = self.ex.group('message')
+            async def name_to_user_object(self, ctx, user: discord.Member):
+                print(type(user)) 
+                return user
 
-        await time.asyncio.sleep(self.dt-DT.datetime.now())
-        embed = discord.Embed(title=self.message)
-        #for x in self.recievers:
-        #    x.send(embed = embed)
-        channel.send(embed = embed)
+            for x in self.recievers:
+                whose_dm = '<@' + x + '>'
+                await name_to_user_object(self, ctx, whose_dm).send(embed=embeded)
+                #await ctx.send(embed = embed)
 
 
     @commands.command(name='remind_format')
